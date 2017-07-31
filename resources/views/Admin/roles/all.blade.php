ead55ce0b9cdadf7b3c1e080d32f68e709ba6481
MS.atayi <ataee.zahra0@gmail.com>
2017-07-31 09:30:39 -0700

    create roles migration -controller -model- view all blade and insert

--- resources/views/Admin/roles/all.blade.php
+++ resources/views/Admin/roles/all.blade.php
@@ -2,34 +2,32 @@
 @section('content')
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <div class="page-header head-section">
-            <h2>کاربران</h2>
+            <h2>مقام ها</h2>
             <div class="btn-group">
-                <a href="{{route('users.create')}}" class="btn btn-sm btn-info">سطوح دسترسی</a>
-                <a href="{{route('users.create')}}" class="btn btn-sm btn-primary">کاربران مدیریت</a>
+                <a href="{{route('roles.create')}}" class="btn btn-sm btn-info">ایجاد مقام </a>
+                <a href="{{route('permissions.create')}}" class="btn btn-sm btn-primary">کاربران مدیریت</a>
             </div>
         </div>
         <div class="table-responsive">
             <table class="table table-striped table-bordered">
                 <thead>
                 <tr>
-                    <th>نام کاربر</th>
-                    <th>ایمیل</th>
-                    <th>وضعیت ایمیل</th>
+                    <th>نام مقام</th>
+                    <th>توضیحات</th>
                     <th>تنظیمات</th>
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($users as $user)
+                @foreach($roles as $role)
                     <tr>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>0</td>
+                        <td>{{$role->name}}</td>
+                        <td>{{$role->label}}</td>
                         <td>
-                            <form action="{{route('users.destroy',['id'=>$user->id])}}" method="post">
+                            <form action="{{route('roles.destroy',['id'=>$role->id])}}" method="post">
                                 {{csrf_field()}}
                                 {{method_field('delete')}}
                                 <div class="btn-group btn-group-xs" >
-                                    <a href="{{route('users.edit',['id'=>$user->id])}}" class="btn btn-success">ویرایش</a>
+                                    <a href="{{route('roles.edit',['id'=>$role->id])}}" class="btn btn-success">ویرایش</a>
                                     <button type="submit" class="btn btn-danger">حذف</button>
                                 </div>
                             </form>
@@ -41,7 +39,7 @@
             </table>
         </div>
         <div style="text-align: center">
-            {!! $users->render() !!}
+            {!! $roles->render() !!}
         </div>
     </div>
 @endsection
