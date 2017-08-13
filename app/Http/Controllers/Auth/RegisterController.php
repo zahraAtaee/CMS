88fd1d54bf221231aed97b9b941be2b0a4f0e203
MS.atayi <ataee.zahra0@gmail.com>
2017-08-13 10:58:30 -0700

    create view and route authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,13 +63,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user=User::create([
+
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
 
-        event(new UserRegistered($user));
-        return $user;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,9 @@
 |
 */
 Route::get('/',function (){
-   $user=\App\User::find(1);
 
-   event(new \App\Events\UserRegistered($user));
+    event(new \App\Events\UserActivation(\App\User::find(2)));
 
-   dd('eeeeeeee');
 });
 Route::namespace('Admin')->prefix('admin')->group(function (){
 
@@ -42,3 +40,22 @@ Route::namespace('Admin')->prefix('admin')->group(function (){
 
 });
 
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
