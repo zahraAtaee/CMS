0e232c2b6a9d4c715dd698663f1c6d9d02d2da0c
MS.atayi <ataee.zahra0@gmail.com>
2017-08-19 14:14:22 -0700

    add token_api in fillabel and hidden

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','active','g-recaptcha-response'
+        'name', 'email', 'password','active','g-recaptcha-response','api_token'
     ];
 
     /**
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token','api_token'
     ];
 
     public function activationCode()
