145212edc671370a078db7a9fd7fc2656d7a19c3
MS.atayi <ataee.zahra0@gmail.com>
2017-09-03 03:30:33 +0430

    create view composer in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Comment;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         Validator::extend('recaptcha',Validation\Recaptcha::class);
+
+        view()->composer('Admin.section.header',function($view){
+
+            $successful=Comment::where('approved',1)->count();
+            $unsuccessful=Comment::where('approved',0)->count();
+            $view->with(['successful'=>$successful,'unsuccessful'=>$unsuccessful]);
+        });
+
 
 
     }
