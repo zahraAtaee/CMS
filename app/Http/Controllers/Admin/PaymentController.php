57b0348e5e73a440cdf05046ac8b147f79770bff
MS.atayi <ataee.zahra0@gmail.com>
2017-09-06 23:47:41 +0430

    create Admin Payment and Activ-Deleted -Success and unSuccess

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Payment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +15,15 @@ class PaymentController extends Controller
      */
     public function index()
     {
-        //
+        $payments=Payment::with('user')->wherePayment(1)->latest()->paginate(20);
+
+        return view('Admin.payments.all',compact('payments'));
+    }
+
+    public function unsuccessful()
+    {
+        $payments=Payment::with('user')->wherePayment(0)->latest()->paginate(20);
+        return view('Admin.payments.unSuccess',compact('payments'));
     }
 
     /**
@@ -24,7 +33,7 @@ class PaymentController extends Controller
      */
     public function create()
     {
-        //
+
     }
 
     /**
@@ -63,23 +72,30 @@ class PaymentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param Payment $payment
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Payment $payment)
     {
-        //
+        $payment->update([
+            'payment'=>1
+        ]);
+        alert()->success('کاربر گرامی ! بروز رسانی با موفقیت انجام شد.','بروز رسانی')->persistent('بسیار خوب :)');
+        return back();
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param Payment $payment
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Payment $payment)
     {
-        //
+        $payment->delete();
+        alert()->error('کاربر گرامی ! حذف با موفقیت انجام شد.','حذف')->persistent('باشه!');
+        return back();
     }
 }
