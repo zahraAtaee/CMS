4a3f96cf347fbb58ca8e72a69ee84ea275234d97
MS.atayi <ataee.zahra0@gmail.com>
2017-09-13 23:28:55 +0430

    create messages for more english

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZAtaee
+ * Date: 9/12/2017
+ * Time: 2:37 AM
+ */
+
+return[
+
+    'title'=>'Customer Relationship Management ',
+    'welcome' =>[
+            'title'=>' Welcom To CMS',
+            'description'=>'Sapien sed tincidunt urna ultrices, vivamus amet montes, justo neque ac donec, non lacus ut nec nulla. In vestibulum tristique purus, orci scelerisque sit natoque vitae, massa sem, vitae proin ac malesuada non, erat odio in. Pellentesque tincidunt sit at tempor. Mauris sodales sed odio, dolor sem mauris sit nec, condimentum malesuada vivamus pede mauris ullamcorper volutpat, augue dictum odio. Sapien eu, fusce sed placerat. Libero interdum adipiscing tincidunt tempus mauris per, nec morbi felis, gravida arcu ut, potenti ante amet in. In litora massa tellus et lectus sollicitudin, sed eu nunc, wisi venenatis amet, voluptas consequat pellentesque vehicula.'
+        ],
+];
